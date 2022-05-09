8a2f21cdb63d8c77cff0c8256617d32c71fbe4ea
kiarashshahedi <shahedikiarash@gmail.com>
2022-05-09 19:31:42 +0430

    mostanad sazi ba schema

--- weblog/urls.py
+++ weblog/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('post.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    path('schema/', get_schema_view(title="Blog Api"))
 ]
